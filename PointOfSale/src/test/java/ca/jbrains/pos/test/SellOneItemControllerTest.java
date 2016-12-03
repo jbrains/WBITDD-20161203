71ba223ce5c00a7df84c6a06d63c6a524ae8fee5
J. B. Rainsberger <me@jbrains.ca>
2016-12-03 16:26:05 +0100

    Our Controller now gracefully handles not finding a price for the barcode.

--- PointOfSale/src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
+++ PointOfSale/src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -4,19 +4,26 @@ import io.atlassian.fugue.Option;
 import lombok.Value;
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class SellOneItemControllerTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
+    private Catalog catalog;
+    private Display display;
+    private SellOneItemController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        catalog = context.mock(Catalog.class);
+        display = context.mock(Display.class);
+        controller = new SellOneItemController(catalog, display);
+    }
 
     @Test
     public void productFound() throws Exception {
-        final Catalog catalog = context.mock(Catalog.class);
-        final Display display = context.mock(Display.class);
-        final SellOneItemController controller = new SellOneItemController(catalog, display);
-
         context.checking(new Expectations() {{
             allowing(catalog).findPrice(with("12345"));
             will(returnValue(Option.some(Price.cents(795))));
@@ -27,12 +34,26 @@ public class SellOneItemControllerTest {
         controller.onBarcode("12345");
     }
 
+    @Test
+    public void productNotFound() throws Exception {
+        context.checking(new Expectations() {{
+            allowing(catalog).findPrice(with("::barcode not found::"));
+            will(returnValue(Option.none()));
+
+            oneOf(display).displayProductNotFoundMessage(with("::barcode not found::"));
+        }});
+
+        controller.onBarcode("::barcode not found::");
+    }
+
     public interface Catalog {
         Option<Price> findPrice(String barcode);
     }
 
     public interface Display {
         void displayPrice(Price price);
+
+        void displayProductNotFoundMessage(String barcodeNotFound);
     }
 
     public static class SellOneItemController {
@@ -45,7 +66,11 @@ public class SellOneItemControllerTest {
         }
 
         public void onBarcode(String barcode) {
-            display.displayPrice(catalog.findPrice(barcode).get());
+            final Option<Price> maybePrice = catalog.findPrice(barcode);
+            if (maybePrice.isEmpty())
+                display.displayProductNotFoundMessage(barcode);
+            else
+                display.displayPrice(maybePrice.get());
         }
     }
 
